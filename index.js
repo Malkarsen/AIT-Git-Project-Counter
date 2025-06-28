469b32f052169d4260fc0dfe0bc53fef7681d261
CureForCode <artursomov@yahoo.de>
2025-06-28 19:54:34 +0200

    Resolved Issue #5
    Updated index.js:
    - Added global settings for the entire script
    - Implemented Reset button functionality
    
    Added comments for other team members to improve better experience and project navigation.
    
    Remaining JS tasks can now be completed easily by leveraging the new global settings and Reset button realisation.
    
    In styles.css added a comment for the person working on the website styling.

---
+++ index.js
@@ -0,0 +1,22 @@
+// Common settings for whole JavaScript
+const counterDisplay = document.getElementById("counter_Value");
+let count = 0;
+
+// Issue 3: Implement "Increase" button logic.
+// Person 3 (edit this line and write your name here)
+
+
+// Issue 4: Implement "Decrease" button logic.
+// Person 4 (edit this line and write your name here)
+
+
+// Issue 5: Implement "Reset" button logic.
+// Artur Somov
+document.getElementById("reset").addEventListener("click", () => {
+  count = 0;
+  counterDisplay.textContent = 0; // сразу показываем 0 на экране
+});
+
+// Issue 6: Validating the counter value
+// Person 6 (edit this line and write your name here)
+
