0065a7f7d67648a9301a475e0bd2643aa7cacf7e
Malkarsen <malko.arina212@gmail.com>
2025-07-01 17:25:55 +0200

    Format code

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ let count = 0;
 
 // Issue 3: Implement "Increase" button logic.
 // Ruslan Semenchik
-INCREMENT_BUTTON.addEventListener("click", ()=>{ 
-count++ ; 
-counterDisplay.textContent = count;
+INCREMENT_BUTTON.addEventListener("click", () => {
+  count++;
+  counterDisplay.textContent = count;
 });
 
 // Issue 4: Implement "Decrease" button logic.
 // Ruslan Semenchik
-DECREMENT_BUTTON.addEventListener("click", ()=>{ 
-count--;    
-counterDisplay.textContent = count;
+DECREMENT_BUTTON.addEventListener("click", () => {
+  count--;
+  counterDisplay.textContent = count;
 });
-
 
 // Issue 5: Implement "Reset" button logic.
 // Artur Somov
@@ -29,7 +28,8 @@ document.getElementById("reset").addEventListener("click", () => {
 // Issue 6: Validating the counter value
 // Anton Cheban
 DECREMENT_BUTTON.addEventListener("click", () => {
-  if (count < 0) { // защита от отрицательных чисел
+  if (count < 0) {
+    // защита от отрицательных чисел
     count = 0;
   }
   counterDisplay.textContent = count;
